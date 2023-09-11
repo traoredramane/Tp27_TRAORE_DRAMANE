73ba84ab05dd70d2b3055a408a58a0840f369d2d
traoredramane <trdramane98@gmail.com>
2023-09-11 16:23:32 +0400

    en cours

--- index.php
+++ index.php
@@ -1,17 +1,67 @@
-
 <?php
 try
 {
-// On se connecte à MySQL
-$bdd = new PDO('mysql:host=localhost;dbname=dramane', 'root', '');
-} 
+$bdd = new PDO('mysql:host=localhost;dbname=fare', 'root', '');
+}
 catch(Exception $e)
 {
-// En cas d'erreur, on affiche un message et on arrête tout
 die('Erreur : '.$e->getMessage());
 }
 ?>
 
-<?php
-include("nav.php")
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    
+    
+</head>
+<body>
+
+<?php include("nav.php") ?>
+   
+  <section class="explo">
+  
+  <h1>Explorez Bobo Dioulasso</h1>
+  <div class="rows ex">
+  
+<div class="explo-col">
+<h1>LA SITARAIL</h1>
+<img src="R.jpg" alt="">
+</div>
+
+<div class="explo-col ex1">
+<h1>ARCHIDIOCESE DE BOBO </h1>
+<img src="eglise.jpg" alt="">
+
+</div>
+
+  </div>
+  
+  
+  <div>
+  <h1>A savoir</h1>
+  <h3>La SITARAIL et ARCHIDIOCESE DE BOBO sont des lieux de refferences pour la ville de bobo</h3>
+  <P>La Société internationale de transport africain par rail (ou SITARAIL) est une société basée à Abidjan, filiale de Bolloré Transport & Logistics opérant les chemins de fer en Côte d'Ivoire et au Burkina Faso. Par ailleur L’ archidiocèse de Bobo-Dioulasso est un archidiocèse de l'Église catholique au Burkina Faso. Son siège est la cathédrale Notre-Dame-de-Lourdes.</P>
+  </div>
+  </section><br><br>
+   
+   
+   
+   
+   
+    
+</body>
+</html>
+
+<?php include("footer.php") ?>
--- nav.php
+++ nav.php
@@ -1,12 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-</body>
-</html>
+<header>
+
+
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid na">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+       
+       
+      <li class="nav-item">
+          <a class="nav-link" href="#">Service</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Langue
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="#">Dioula</a></li>
+            <li><a class="dropdown-item" href="#">Moorée</a></li>
+            <li><a class="dropdown-item" href="#">Peulh</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="bo">
+<h2>Bobo_dioulasso"une ville de diversité culturelle qui regorge plusieurs <br> Site Touristiques"</h2>
+
+</div>
+
+</header><br><br>
