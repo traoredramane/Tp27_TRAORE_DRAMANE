35fe842cdeda0f1d8994662f4e32220dbef601a9
traoredramane <trdramane98@gmail.com>
2023-09-11 11:24:59 +0400

    debut du projet

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+try
+{
+// On se connecte à MySQL
+$bdd = new PDO('mysql:host=localhost;dbname=dramane', 'root', '');
+} 
+catch(Exception $e)
+{
+// En cas d'erreur, on affiche un message et on arrête tout
+die('Erreur : '.$e->getMessage());
+}
+?>
+
+<?php
+include("nav.php")
+?>
